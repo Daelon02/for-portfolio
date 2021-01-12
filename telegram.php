31f073dedb532f3b1b68cf72354980a9f7c7054b
Nikita <63471940+Daelon02@users.noreply.github.com>
2021-01-12 04:24:33 +0200

    Update form

--- telegram.php
+++ telegram.php
@@ -1,9 +1,9 @@
 <?php
  
 //Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
-$name = $_POST['Name'];
-$email = $_POST['Email'];
-$title = $_POST['Title'];
+$name = $_POST['user-name'];
+$email = $_POST['user_email'];
+$title = $_POST['user_title'];
  
 //в переменную $token нужно вставить токен, который нам прислал @botFather
 $token = "1509208531:AAGtyzlfLldE1OTEJtggnyTv3N3zjHTw_7A";
@@ -19,9 +19,12 @@ $arr = array(
 );
  
 //При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
+$txt = "<pre>";
 foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
+    $txt .= "<b>".$key."</b>".$value."\n".$title;
 };
+$txt .= "</pre>";
+$txt = urlencode($txt);
  
 //Осуществляется отправка данных в переменной $sendToTelegram
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
